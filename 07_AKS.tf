0c99b131b34d93d21c7c870fc905689935fb8e29
Patryk Sk <patryk@skrz.int.pl>
2025-12-16 00:01:16 +0100

    feat(aks): add basic AKS

---
+++ 07_AKS.tf
@@ -0,0 +1,37 @@
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "${var.project_name}-aks"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "${var.project_name}aksdns99"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_B2s"
+    auto_scaling_enabled = true
+    min_count            = 1
+    max_count            = 2
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin = "azure"
+    network_policy = "azure"
+  }
+
+  tags = var.common_tags
+}
+
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate
+  sensitive = true
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.aks.kube_config_raw
+
+  sensitive = true
+}
