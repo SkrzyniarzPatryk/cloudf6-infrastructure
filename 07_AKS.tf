b8229f014869d398f6b0e420bd175a314bc59962
Patryk Sk <patryk@skrz.int.pl>
2025-12-17 19:16:35 +0100

    feat(aks): Add AGIC ingress with roles

--- 07_AKS.tf
+++ 07_AKS.tf
@@ -40,21 +40,43 @@ resource "azurerm_kubernetes_cluster" "aks" {
     type = "SystemAssigned"
   }
 
+  ingress_application_gateway {
+    gateway_id = azurerm_application_gateway.network.id
+  }
+
   network_profile {
     network_plugin = "azure"
     network_policy = "azure"
   }
 
   tags = var.common_tags
+
+  lifecycle {
+    ignore_changes = [
+      default_node_pool[0].upgrade_settings # ignore upgrade_settings
+    ]
+  }
 }
 #########################
-# ACR role assigned to AKS
+# Role assignment for AKS
 #########################
+# ACR role assigned to AKS
 resource "azurerm_role_assignment" "acr_pull_assignment" {
   scope                = azurerm_container_registry.acr.id
   role_definition_name = "AcrPull"
   principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
-  # principal_id         = azurerm_kubernetes_cluster.aks.identity[0].principal_id
+}
+# Role for AGIC to subnet contribute
+resource "azurerm_role_assignment" "agic_subnet_permission" {
+  scope                = azurerm_subnet.default_sn.id
+  role_definition_name = "Network Contributor"
+  principal_id         = azurerm_kubernetes_cluster.aks.ingress_application_gateway[0].ingress_application_gateway_identity[0].object_id
+}
+# Rolle for managing App Gateway by ingress AGIC
+resource "azurerm_role_assignment" "agic_appgw_contributor" {
+  scope                = azurerm_application_gateway.network.id
+  role_definition_name = "Contributor"
+  principal_id         = azurerm_kubernetes_cluster.aks.ingress_application_gateway[0].ingress_application_gateway_identity[0].object_id
 }
 
 #########################
