e99b62ab6bc916dc6332a31a752c8684482ff04a
Patryk Sk <patryk@skrz.int.pl>
2025-12-18 17:45:40 +0100

    feat: add pgress sql

--- 06a_vnet.tf
+++ 06a_vnet.tf
@@ -19,9 +19,20 @@ resource "azurerm_subnet" "aks_sn" {
   address_prefixes     = ["10.10.20.0/24"]
 }
 
+# Subnet for postgres flexible server 
 resource "azurerm_subnet" "db_sn" {
   name                 = "db-sn"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.10.30.0/24"]
+  service_endpoints    = ["Microsoft.Storage"]
+  delegation {
+    name = "fs"
+    service_delegation {
+      name = "Microsoft.DBforPostgreSQL/flexibleServers"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/join/action",
+      ]
+    }
+  }
 }
---
+++ 09_postgres_flexible_server.tf
@@ -0,0 +1,31 @@
+resource "azurerm_private_dns_zone" "private_dns" {
+  name                = "europe.postgres.database.azure.com"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "private_dns_link" {
+  name                  = "exampleVnetZone.com"
+  private_dns_zone_name = azurerm_private_dns_zone.private_dns.name
+  virtual_network_id    = azurerm_virtual_network.vnet.id
+  resource_group_name   = azurerm_resource_group.rg.name
+  depends_on            = [azurerm_subnet.db_sn]
+}
+
+resource "azurerm_postgresql_flexible_server" "psql_server" {
+  name                          = "javaserviceapppsqlflexserver"
+  location                      = azurerm_resource_group.rg.location
+  resource_group_name           = azurerm_resource_group.rg.name
+  version                       = "16"
+  delegated_subnet_id           = azurerm_subnet.db_sn.id
+  private_dns_zone_id           = azurerm_private_dns_zone.private_dns.id
+  public_network_access_enabled = false
+  administrator_login           = "psqladmin"
+  administrator_password        = "H@Sh1CoR3!" 
+  # zone                          = "1"
+
+  storage_mb   = 32768
+  storage_tier = "P4"
+
+  sku_name   = "B_Standard_B1ms"
+  depends_on = [azurerm_private_dns_zone_virtual_network_link.private_dns_link]
+}
