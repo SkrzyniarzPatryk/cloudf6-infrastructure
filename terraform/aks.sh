7b1870034f0665d96b541429e3c313006e4b434d
Patryk Sk <patryk@skrz.int.pl>
2025-12-25 19:52:42 +0100

    refactor(terraform): update script

--- terraform/aks.sh
+++ terraform/aks.sh
@@ -9,18 +9,15 @@ fi
 
 # targets
 targets=(
-  "-target=azurerm_application_gateway.network"
-  # "-target=azurerm_container_registry.acr"
-  "-target=azurerm_kubernetes_cluster.aks"
-  "-target=azurerm_public_ip.appgw_public_ip"
-  # "-target=azurerm_resource_group.rg"
-  "-target=azurerm_role_assignment.acr_pull_assignment"
-  "-target=azurerm_role_assignment.agic_subnet_permission"
-  "-target=azurerm_role_assignment.agic_appgw_contributor"
-  # "-target=azurerm_role_assignment.ra_reader"
+  "-target=module.aks_and_appgw"
+  "-target=azurerm_virtual_network.vnet"
+  "-target=azurerm_subnet.appgw_sn"
   "-target=azurerm_subnet.aks_sn"
-  "-target=azurerm_subnet.default_sn"
-  # "-target=azurerm_virtual_network.vnet"
+  "-target=azurerm_subnet.db_sn"
+  "-target=azurerm_postgresql_flexible_server.psql_server"
+  "-target=azurerm_postgresql_flexible_server_database.psql_server"
+  "-target=azurerm_private_dns_zone.private_dns"
+  "-target=azurerm_private_dns_zone_virtual_network_link.private_dns_link"
 )
 
 # apply / destroy command
